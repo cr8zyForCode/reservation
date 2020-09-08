589bf2f8c82c30c328bfc8d2d2867aea073a7008
Jorge Garcia <jorgarcia1994@gmail.com>
2020-09-08 16:09:53 -0700

    updated index.js

--- database/index.js
+++ database/index.js
@@ -4,11 +4,16 @@ const cassandra = require('cassandra-driver');
 // const client = new cassandra.Client({
 //   contactPoints: ['host1', 'host2'],
 //   localDataCenter: 'datacenter1'
-// });
+// });'172.31.13.142',
+
+var authProvider = new cassandra.auth.PlainTextAuthProvider('root', 'root');
+
+console.log('hello')
 
 const client = new cassandra.Client({
-  contactPoints: ['127.0.0.1'],
+  contactPoints: ['54.151.34.180'],
   keyspace: 'calendar',
+  authProvider: authProvider,
   localDataCenter: 'datacenter1'
 });
 
@@ -16,12 +21,23 @@ client.connect((err) => {
   if (err) {
     console.log(err)
   } else {
-    console.log('connected to cassandra')
+    console.log('connected to cassandra');
   }
-  // assert.ifError(err);
 });
 
-// const query = "SELECT * FROM reservations WHERE property_id = 9999999";
+
+
+// client.execute(query, (err, result) => {
+//   if (err) {
+//     console.log('could not get reservations');
+//     cb(err)
+//   } else {
+//     // console.log('got the data', resu);
+//     // cb(null, result)
+//   }
+// });
+// };
+
 
 // client.execute(query, function (err, result) {
 //   // var reservations = result.first();
@@ -29,3 +45,12 @@ client.connect((err) => {
 //   console.log('objectOfReservations', result.rows);
 // });
 module.exports = client;
+
+// var assert = require('assert');
+// //”cassandra-driver” is in the node_modules folder. Redirect if necessary.
+// var cassandra = require('cassandra-driver');
+// //Replace Username and Password with your cluster settings
+// var authProvider = new cassandra.auth.PlainTextAuthProvider('Username', 'Password');
+// //Replace PublicIP with the IP addresses of your clusters
+// var contactPoints = ['PublicIP', 'PublicIP', 'PublicIP’'];
+// var client = new cassandra.Client({ contactPoints: contactPoints, authProvider: authProvider, keyspace: 'grocery' });
